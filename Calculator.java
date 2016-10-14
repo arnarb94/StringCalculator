92407c4fe6f070694bbf0d8a55537305052d46cb
Arnarp15 <arnarp15@ru.is>
2016-10-14 23:26:01 +0000

    fixed and added functions in Calculator.java to parse Integers

--- Calculator.java
+++ Calculator.java
@@ -13,6 +13,14 @@ public class Calculator {
 			return 1;
 	}
 
+	private static int toInt(String number) {
+		return Integer.parseInt(number);
+	}
+
+	private static String[] splitNumbers(String numbers) {
+		return numbers.split(",");
+	}
+
 	private static int sum(String [] numbers) {
 		int total = 0;
 		for(String number : numbers)
